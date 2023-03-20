85a1162f9cb3466be70588bce1cf8c62d742f91d
azazaz0223 <ray0961331980@gmail.com>
2023-03-20 17:58:04 +0800

    feat: 可以建立初始超級管理員與permission的資料

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Cate;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,15 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run() : void
     {
-        \Schema::disableForeignKeyConstraints();
         User::truncate();
         Cate::truncate();
         Product::truncate();
 
-        User::factory(5)->create();
+        // 建立最高權限管理員
+        User::create([
+            'name' => 'Admin',
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => bcrypt('1111'),
+            'remember_token' => Str::random(10),
+        ]);
+        User::factory(4)->create();
         Cate::factory(100)->create();
         Product::factory(10000)->create();
-
-        \Schema::enableForeignKeyConstraints();
     }
 }
